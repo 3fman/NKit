3ce7feeab70fa42805907198b35380c0c6abaf6f
Bruce Lee <liuwei.li@outlook.com>
2024-03-07 21:40:31 +0800

    。

---
+++ NKit.CSharp/TimerX.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Threading;
+
+namespace NKit.CSharp
+{
+    public class TimerX : IDisposable
+    {
+        private TimeSpan _periodTime;
+
+        private readonly System.Threading.Timer _timer;
+
+        public TimerX(TimerCallback callback)
+        {
+            _timer = new Timer((state) =>
+            {
+                callback?.Invoke(state);
+                _timer?.Change(_periodTime, Timeout.InfiniteTimeSpan);
+            });
+        }
+
+        public void Change(TimeSpan period)
+        {
+            if (period == Timeout.InfiniteTimeSpan)
+            {
+                WaitForDispose(_timer, TimeSpan.MaxValue);
+            }
+            else
+            {
+                _periodTime = period;
+            }
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        private bool WaitForDispose(Timer timer, TimeSpan timeout)
+        {
+            timer.Change(Timeout.InfiniteTimeSpan, Timeout.InfiniteTimeSpan); // 暂停派送新任务
+            if (timeout != TimeSpan.Zero)
+            {
+                ManualResetEvent waitHandle = new ManualResetEvent(false);
+                if (timer.Dispose(waitHandle) && !waitHandle.WaitOne((int)timeout.TotalMilliseconds))
+                {
+                    return false;
+                }
+                waitHandle.Close();
+            }
+            else
+            {
+                timer.Dispose();
+            }
+            return true;
+        }
+    }
+}
